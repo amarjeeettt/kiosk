ee8432861b09fa1a9e71ec20760e2cea5a6a8809
Amarjeet Singh <amarjeet060730@gmail.com>
2024-04-24 21:33:45 +0800

    GPIO pin reset code added

--- test.py
+++ test.py
@@ -15,3 +15,28 @@ try:
         time.sleep(1)  # Keep the program running
 except KeyboardInterrupt:
     print("Program terminated by user")
+
+
+
+"""
+import RPi.GPIO as GPIO
+import time
+
+# Set up GPIO using BCM numbering
+GPIO.setmode(GPIO.BCM)
+
+# Pin number to reset
+pin_number = 17
+
+# Set pin as input and pull it low (reset)
+GPIO.setup(pin_number, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Wait for a short duration
+time.sleep(0.1)
+
+# Set pin back to output mode and pull it low (if necessary)
+GPIO.setup(pin_number, GPIO.OUT, initial=GPIO.LOW)
+
+# Clean up GPIO
+GPIO.cleanup()
+"""
